47f6ed7fb5ee72ca0b0f9b4ecbbd83a0e8edd673
Alberto Fernández Capel <afcapel@gmail.com>
2011-08-10 01:29:26 +0200

    uncommented tests

--- test/models/event_observer_test.rb
+++ test/models/event_observer_test.rb
@@ -66,61 +66,61 @@ module PushyResources
 
   class EventObserverTest < ActiveSupport::TestCase
 
-    # test "can observe specifying a string as channel pattern" do
-    #   class TextPatternObserver < EventObserver
-    #     observe 'string pattern'
-    #   end
-    #
-    #   assert  TextPatternObserver.observe?('string pattern')
-    #   assert  TextPatternObserver.observe?('string pattern and more')
-    #   assert !TextPatternObserver.observe?('other string pattern')
-    # end
-    #
-    # test "can observe specifying a regexp as channel pattern" do
-    #   class RegexpPatternObserver < EventObserver
-    #     observe /man$/
-    #   end
-    #
-    #   assert  RegexpPatternObserver.observe?('Superman')
-    #   assert !RegexpPatternObserver.observe?('Lex Luthor')
-    # end
-    #
-    # test "it has a default channel pattern" do
-    #   class DefaultPatternsObserver < EventObserver; end
-    #
-    #   assert  DefaultPatternsObserver.observe?('/default/patterns/')
-    #   assert  DefaultPatternsObserver.observe?('/default/patterns/1')
-    #
-    #   assert !DefaultPatternsObserver.observe?('/default/other/')
-    #   assert !DefaultPatternsObserver.observe?('/other/patterns/')
-    # end
-    #
-    # test "default channel pattern is ignored if explicit observe pattern is called" do
-    #   class OverwrittenDefaultPatternsObserver < EventObserver
-    #     observe '/others'
-    #   end
-    #
-    #   assert  OverwrittenDefaultPatternsObserver.observe?('/others')
-    #   assert  OverwrittenDefaultPatternsObserver.observe?('/others/1/')
-    #   assert !OverwrittenDefaultPatternsObserver.observe?('/overwritten/default/patterns')
-    # end
-    #
-    #
-    # test 'receive created and destroyes events' do
-    #   ChatObserver.observe '/chats/'
-    #
-    #   chat = Chat.create :name => 'Observed chat'
-    #
-    #   sleep(0.1)
-    #
-    #   assert ChatObserver.created_chats.include?(chat)
-    #
-    #   chat.destroy
-    #
-    #   sleep(0.1)
-    #
-    #   assert !ChatObserver.created_chats.include?(chat)
-    # end
+    test "can observe specifying a string as channel pattern" do
+      class TextPatternObserver < EventObserver
+        observe 'string pattern'
+      end
+
+      assert  TextPatternObserver.observe?('string pattern')
+      assert  TextPatternObserver.observe?('string pattern and more')
+      assert !TextPatternObserver.observe?('other string pattern')
+    end
+
+    test "can observe specifying a regexp as channel pattern" do
+      class RegexpPatternObserver < EventObserver
+        observe /man$/
+      end
+
+      assert  RegexpPatternObserver.observe?('Superman')
+      assert !RegexpPatternObserver.observe?('Lex Luthor')
+    end
+
+    test "it has a default channel pattern" do
+      class DefaultPatternsObserver < EventObserver; end
+
+      assert  DefaultPatternsObserver.observe?('/default/patterns/')
+      assert  DefaultPatternsObserver.observe?('/default/patterns/1')
+
+      assert !DefaultPatternsObserver.observe?('/default/other/')
+      assert !DefaultPatternsObserver.observe?('/other/patterns/')
+    end
+
+    test "default channel pattern is ignored if explicit observe pattern is called" do
+      class OverwrittenDefaultPatternsObserver < EventObserver
+        observe '/others'
+      end
+
+      assert  OverwrittenDefaultPatternsObserver.observe?('/others')
+      assert  OverwrittenDefaultPatternsObserver.observe?('/others/1/')
+      assert !OverwrittenDefaultPatternsObserver.observe?('/overwritten/default/patterns')
+    end
+
+
+    test 'receive created and destroyes events' do
+      ChatObserver.observe '/chats/'
+
+      chat = Chat.create :name => 'Observed chat'
+
+      sleep(0.1)
+
+      assert ChatObserver.created_chats.include?(chat)
+
+      chat.destroy
+
+      sleep(0.1)
+
+      assert !ChatObserver.created_chats.include?(chat)
+    end
 
     test 'react to subscribed and unsubscribed events' do
       user = Factory.create :user
@@ -141,59 +141,58 @@ module PushyResources
       assert !ChatObserver.subscribed_clients.include?(user)
     end
 
-    # test 'react to subscribed and unsubscribed events on collection' do
-    #   user = Factory.create :user
-    #   connection = MockConnection.new(:id => user.id)
-    #
-    #   assert !ChatObserver.subscribed_to_collection.include?(user)
-    #   assert !ChatObserver.subscribed_to_member.include?(user)
-    #
-    #   Command.new(connection, :command => 'subscribe', :channel => '/chats/').execute!
-    #
-    #   sleep(0.1)
-    #
-    #   assert ChatObserver.subscribed_to_collection.include?(user)
-    #   assert !ChatObserver.subscribed_to_member.include?(user)
-    #
-    #   Command.new(connection, :command => 'unsubscribe', :channel => '/chats/').execute!
-    #
-    #   sleep(0.1)
-    #
-    #   assert !ChatObserver.subscribed_to_collection.include?(user)
-    #   assert !ChatObserver.subscribed_to_member.include?(user)
-    # end
-    #
-    # test 'react to subscribed and unsubscribed events on member' do
-    #   user = Factory.create :user
-    #   connection = MockConnection.new(:id => user.id)
-    #   chat = Factory.create :chat
-    #
-    #   chat_channel = "/chats/#{chat.id}"
-    #
-    #   assert !ChatObserver.subscribed_to_collection.include?(user)
-    #   assert !ChatObserver.subscribed_to_member.include?(user)
-    #
-    #   Command.new(connection, :command => 'subscribe', :channel => chat_channel).execute!
-    #
-    #   sleep(0.1)
-    #
-    #   assert !ChatObserver.subscribed_to_collection.include?(user)
-    #   assert ChatObserver.subscribed_to_member.include?(user)
-    #
-    #   Command.new(connection, :command => 'unsubscribe', :channel => chat_channel).execute!
-    #
-    #   sleep(0.1)
-    #
-    #   assert !ChatObserver.subscribed_to_collection.include?(user)
-    #   assert !ChatObserver.subscribed_to_member.include?(user)
-    # end
-    #
-    # test 'receive customs events' do
-    #   event = Event.new :event => :custom, :resource => Chat.new, :channel => '/chats/'
-    #   EventRouter.process(event)
-    #
-    #   assert_equal ChatObserver.custom_events.last, event
-    # end
+    test 'react to subscribed and unsubscribed events on collection' do
+      user = Factory.create :user
+      connection = MockConnection.new(:id => user.id)
 
+      assert !ChatObserver.subscribed_to_collection.include?(user)
+      assert !ChatObserver.subscribed_to_member.include?(user)
+
+      Command.new(connection, :command => 'subscribe', :channel => '/chats/').execute!
+
+      sleep(0.1)
+
+      assert ChatObserver.subscribed_to_collection.include?(user)
+      assert !ChatObserver.subscribed_to_member.include?(user)
+
+      Command.new(connection, :command => 'unsubscribe', :channel => '/chats/').execute!
+
+      sleep(0.1)
+
+      assert !ChatObserver.subscribed_to_collection.include?(user)
+      assert !ChatObserver.subscribed_to_member.include?(user)
+    end
+
+    test 'react to subscribed and unsubscribed events on member' do
+      user = Factory.create :user
+      connection = MockConnection.new(:id => user.id)
+      chat = Factory.create :chat
+
+      chat_channel = "/chats/#{chat.id}"
+
+      assert !ChatObserver.subscribed_to_collection.include?(user)
+      assert !ChatObserver.subscribed_to_member.include?(user)
+
+      Command.new(connection, :command => 'subscribe', :channel => chat_channel).execute!
+
+      sleep(0.1)
+
+      assert !ChatObserver.subscribed_to_collection.include?(user)
+      assert ChatObserver.subscribed_to_member.include?(user)
+
+      Command.new(connection, :command => 'unsubscribe', :channel => chat_channel).execute!
+
+      sleep(0.1)
+
+      assert !ChatObserver.subscribed_to_collection.include?(user)
+      assert !ChatObserver.subscribed_to_member.include?(user)
+    end
+
+    test 'receive customs events' do
+      event = Event.new :event => :custom, :resource => Chat.new, :channel => '/chats/'
+      EventRouter.process(event)
+
+      assert_equal ChatObserver.custom_events.last, event
+    end
   end
 end
