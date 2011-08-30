04e375931b9b42cda8fb3864d18f848c1ed83a39
Alberto Fernández Capel <afcapel@gmail.com>
2011-08-30 21:26:44 +0200

    Added helpers for creating clients

--- app/helpers/alondra_helper.rb
+++ app/helpers/alondra_helper.rb
@@ -1,4 +1,22 @@
 module AlondraHelper
+
+  def alondra_subscribe_tag(resources)
+    javascript_tag do
+      %Q{
+        $(function(){
+          #{alondra_subscribe(resources)}
+        });
+      }
+    end
+  end
+
+  def alondra_subscribe(resources)
+    resources = [resources] unless Enumerable === resources
+    resources_paths = resources.collect { |r| "'#{polymorphic_path(r)}'" }.join(', ')
+
+    "new AlondraClient('#{Alondra::Alondra.config.host}', #{Alondra::Alondra.config.port}, [#{resources_paths}]);"
+  end
+
   def encrypted_token
     token = {:user_id => current_user.id, :valid_until => 5.minutes.from_now}.to_json
     Alondra::CredentialsParser.verifier.generate(token)
