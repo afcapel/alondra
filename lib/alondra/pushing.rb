548665ef07b6d5ab0ea10ad7b119e0e8d63863d8
Alberto Fernández Capel <afcapel@gmail.com>
2011-09-01 03:03:33 +0200

    Raising custom exception when trying to push to unknown channel

--- lib/alondra/pushing.rb
+++ lib/alondra/pushing.rb
@@ -1,7 +1,10 @@
 module Alondra
+
+  class PushingException < StandardError; end
+
   module Pushing
     def push(options)
-      raise 'You need to specify the channel to push' unless options[:to]
+      raise PushingException.new('You need to specify the channel to push') unless options[:to]
 
       channels = Channel.for(options.delete(:to))
       controller = PushController.new(self)
