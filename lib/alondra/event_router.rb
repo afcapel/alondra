772c1195746f9f5c8bf5ecf3a7a719a25795f1ee
Alberto Fernández Capel <afcapel@gmail.com>
2011-09-04 01:55:35 +0200

    Ensure EM threads don't exaust AR connection pool

--- lib/alondra/event_router.rb
+++ lib/alondra/event_router.rb
@@ -9,10 +9,16 @@ module Alondra
       event.channel.receive(event)
 
       EM.defer do
-        listening_classes = EventRouter.listeners.select do |ob|
-          ob.listen_to?(event.channel_name)
+
+        # Ensure the connection associated with the thread is checked in
+        # after the callbacks are processed
+        ActiveRecord::Base.connection_pool.with_connection do
+          listening_classes = EventRouter.listeners.select do |ob|
+            ob.listen_to?(event.channel_name)
+          end
+
+          listening_classes.each { |listening_class| listening_class.process(event) }
         end
-        listening_classes.each { |listening_class| listening_class.process(event) }
       end
     end
   end
