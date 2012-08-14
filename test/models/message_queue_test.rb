8d0916d45d9b24c52644a912903da3889a361d09
Alberto Fernandez-Capel <afcapel@gmail.com>
2012-08-14 11:22:07 +0200

    Oops, uncomment test

--- test/models/message_queue_test.rb
+++ test/models/message_queue_test.rb
@@ -17,28 +17,28 @@ module Alondra
       MessageQueue.instance.instance_variable_set :@event_router, @original_event_router
     end
 
-    # test "a message pushed asynchronously to the queue is received by the event router" do
-    #   assert MessageQueueClient.instance.class == AsyncMessageQueueClient
-    # 
-    #   MessageQueueClient.push @event
-    # 
-    #   sleep(0.1)
-    # 
-    #   assert received(@event)
-    # end
-    # 
-    # test "a message pushed synchronously to the queue is received by the event router" do
-    # 
-    #   client = MessageQueueClient.sync_instance
-    #   context = client.send :context
-    #   assert context.class == ZMQ::Context
-    # 
-    #   client.send_message(@event)
-    # 
-    #   sleep(0.1)
-    # 
-    #   assert received(@event)
-    # end
+    test "a message pushed asynchronously to the queue is received by the event router" do
+      assert MessageQueueClient.instance.class == AsyncMessageQueueClient
+    
+      MessageQueueClient.push @event
+    
+      sleep(0.1)
+    
+      assert received(@event)
+    end
+    
+    test "a message pushed synchronously to the queue is received by the event router" do
+    
+      client = MessageQueueClient.sync_instance
+      context = client.send :context
+      assert context.class == ZMQ::Context
+    
+      client.send_message(@event)
+    
+      sleep(0.1)
+    
+      assert received(@event)
+    end
 
     test "message queue still works when an exception is thrown while processing an event" do
       3.times do
